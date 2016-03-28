004353ea712966035d81704688a33c372a013e38
Misha <m.danilevskiy@gmail.com>
2016-03-29 00:29:06 +0300

    action buttons implemented

--- views/site/register.php
+++ views/site/register.php
@@ -13,7 +13,7 @@ echo $form->field($model,'passwordHash')->passwordInput()->label('Password');
 echo $form->field($model,'passwordConfirm')->passwordInput()->label('Password Confirm');
 echo $form->field($model,'role')->dropDownList(['user'=>'user','teacher'=>'teacher','student'=>'student','admin'=>'admin']);
 ?>
-<div class="g-recaptcha" data-sitekey="<?=Yii::$app->params['recaptcha']['key']?>" data-size="compact"></div>
+<div class="g-recaptcha" data-sitekey="<?=Yii::$app->params['recaptcha']['key']?>" data-size="normal"></div>
 <?php
 echo Html::submitButton('Submit');
 
--- views/user/list.php
+++ views/user/list.php
@@ -2,6 +2,7 @@
 
 use yii\grid\GridView;
 use yii\helpers\Html;
+use yii\grid\ActionColumn;
 
 echo GridView::widget([
     'dataProvider' => $provider,
@@ -14,7 +15,7 @@ echo GridView::widget([
         'email',
         'role',
         'avatar',
-        [
+/*        [
             'header' => 'Edit',
             'content' => function ($model) {
                 return \yii\bootstrap\Html::a('Edit', ['/user/edit', 'id' => $model->id]);
@@ -25,7 +26,31 @@ echo GridView::widget([
             'content' => function ($model) {
                 return \yii\bootstrap\Html::a('Delete', ['/user/delete', 'id' => $model->id]);
             }
-        ],
+        ],*/
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'header' => 'Actions',
+            'template' => '{update} {delete}',
+            'buttons' => [
+                'update' => function ($url, $model, $key) {
+                    $options = [
+                        'title' => Yii::t('yii', 'Update'),
+                        'aria-label' => Yii::t('yii', 'Update'),
+                        'data-pjax' => '0',
+                    ];
+                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['/user/edit', 'id' => $model->id], $options);
+                },
+                'delete' => function ($url, $model, $key) {
+                    $options = [
+                        'title' => Yii::t('yii', 'Delete'),
+                        'aria-label' => Yii::t('yii', 'Delete'),
+                        'data-pjax' => '0',
+                    ];
+                    return Html::a('<span class="glyphicon glyphicon-trash"></span>', ['/user/delete', 'id' => $model->id], $options);
+                }
+            ],
+
+        ]
     ]
 ]);
 
